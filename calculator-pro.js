6d1e4b704f6bf193979189c9c57ab3678d5fc21c
Damian Padilla <damipad4@gmail.com>
2022-08-05 23:29:25 -0500

    Adding WIP before closing project

---
+++ calculator-pro.js
@@ -0,0 +1,16 @@
+const value1 = document.querySelector(".value1")
+const value2 = document.querySelector(".value2")
+
+value1.addEventListener("click", e => {
+    console.log(e.target);
+})
+
+value2.addEventListener("click", e => {
+    console.log(e);
+})
+
+// const calculate = getElementById("calculate").addEventListener("click", e => {
+//     console.log(e.target)
+// })
+// const value1 = document.getElementById;
+
