e229f592ba02b0568cb07a2da65881fd3875772b
Damian Padilla <damipad4@gmail.com>
2022-08-15 21:47:29 -0500

    Adding PC Updates

--- calculator-pro.js
+++ calculator-pro.js
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-// const value1 = document.querySelector(".value1")
-// const value2 = document.querySelector(".value2")
-
-// value1.addEventListener("click", e => {
-//     console.log(e.target);
-// })
-
-// value2.addEventListener("click", e => {
-//     console.log(e.target);
-=======
-const value1 = document.querySelector(".value1")
-const value2 = document.querySelector(".value2")
-const btn = document.querySelector("btn")
+let n1 = parseInt(document.getElementById("num").value);
 
 
-
-function getInput() {
-  let input = document.getElementById("value1").value1
-  console.log(input);
-}
-
-
-  btn.addEventListener("click", e => {
-    console.log(getInput(e));
-
-
-
-// value2.addEventListener("click", e => {
-//   console.log(e);
->>>>>>> 2b7fa57722d769873c0d85a9990c1df3a4c02925
-// })
-
-// const calculate = getElementById("calculate").addEventListener("click", e => {
-//     console.log(e.target)
-// })
-// const value1 = document.getElementById;
 
