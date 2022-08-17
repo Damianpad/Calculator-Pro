79a801e86e83a7e8e151fe2004f0fd95a2d3c05d
Damian Padilla <damipad4@gmail.com>
2022-08-17 13:38:14 -0500

    Updating PC

--- calculator-pro.js
+++ calculator-pro.js
@@ -1,93 +1,92 @@
-
 const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
 };
 
 function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
+    const { displayValue, waitingForSecondOperand } = calculator;
 
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-  }
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
 
-  console.log(calculator);
+    console.log(calculator);
 }
 
 function inputDecimal(dot) {
-  // If the `displayValue` property does not contain a decimal point
-  if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
-    calculator.displayValue += dot;
-  }
+    // If the `displayValue` property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+        // Append the decimal point
+        calculator.displayValue += dot;
+    }
 }
 
 function handleOperator(nextOperator) {
-  // Destructure the properties on the calculator object
-  const { firstOperand, displayValue, operator } = calculator
-  // `parseFloat` converts the string contents of `displayValue`
-  // to a floating-point number
-  const inputValue = parseFloat(displayValue);
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator
+    // `parseFloat` converts the string contents of `displayValue`
+    // to a floating-point number
+    const inputValue = parseFloat(displayValue);
 
-  // verify that `firstOperand` is null and that the `inputValue`
-  // is not a `NaN` value
-  if (firstOperand === null && !isNaN(inputValue)) {
-    // Update the firstOperand property
-    calculator.firstOperand = inputValue;
-  }
+    // verify that `firstOperand` is null and that the `inputValue`
+    // is not a `NaN` value
+    if (firstOperand === null && !isNaN(inputValue)) {
+        // Update the firstOperand property
+        calculator.firstOperand = inputValue;
+    }
 
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-  console.log(calculator);
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
 }
 
 function updateDisplay() {
-  // select the element with class of `valueBox`
-  const display = document.querySelector('.valueBox');
-  // update the value of the element with the contents of `displayValue`
-  display.value = calculator.displayValue;
+    // select the element with class of `valueBox`
+    const display = document.querySelector('.valueBox');
+    // update the value of the element with the contents of `displayValue`
+    display.value = calculator.displayValue;
 }
 
 updateDisplay();
 
 const keys = document.querySelector('.numPad');
 keys.addEventListener('click', (event) => {
-  // Access the clicked element
-  const { target } = event;
+    // Access the clicked element
+    const { target } = event;
 
-  // Check if the clicked element is a button.
-  // If not, exit from the function
-  if (!target.matches('button')) {
-    return;
-  }
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
 
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        updateDisplay();
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
     updateDisplay();
-    console.log('operator', target.value);
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-    updateDisplay();
-    console.log('decimal', target.value);
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-  console.log('digit', target.value)
+    console.log('digit', target.value)
 });
 
 // let value1 = document.querySelector('#valueBox');
