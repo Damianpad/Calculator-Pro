d9efc15b262a485b63fb66de063cb629e00778aa
Damian Padilla <damipad4@gmail.com>
2022-08-16 20:59:51 -0500

    Added logic for calculator and updated class names in html

--- calculator-pro.js
+++ calculator-pro.js
@@ -1,8 +1,115 @@
-const value1 = document.getElementById("valueBox");
-const btn = document.getElementById("num");
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+
+  console.log(calculator);
+}
+
+function inputDecimal(dot) {
+  // If the `displayValue` property does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function updateDisplay() {
+  // select the element with class of `valueBox`
+  const display = document.querySelector('.valueBox');
+  // update the value of the element with the contents of `displayValue`
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.numPad');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+    updateDisplay();
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+    updateDisplay();
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+  console.log('digit', target.value)
+});
+
+// let value1 = document.querySelector('#valueBox');
+// let btn = document.querySelectorAll('.num');
+// let btn7 = document.querySelector('#num7');
+// let btn8 = document.querySelector('#num8');
+
+// btn.addEventListener('click', function handleClick() {
+//   calculator.displayValue = document.querySelector('#num7').value;
+//   value1.value =  calculator.displayValue
+//   btn7.style.color = '#f16868';
+// })
 
 
-btn.addEventListener('click', function handleClick() {
-    value1. = '7';
-})
+// btn7.addEventListener('click', function handleClick() {
+//   calculator.displayValue = '7'
+//   value1.value =  calculator.displayValue
+//   btn7.style.color = '#f16868';
+// })
+
+// btn8.addEventListener('click', function handleClick() {
+//   calculator.displayValue = '8'
+//   value1.value =  calculator.displayValue
+//   btn7.style.color = '#f16868';
+// })
 
